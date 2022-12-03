1889cda3dcb12f16b0cc506ad05f6014fc956159
Miwka <97275239+albertu4@users.noreply.github.com>
2022-12-03 19:17:27 +0700

    Initial Commit

---
+++ MyDictionary/MyDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDictionaryApp.swift
+//  MyDictionary
+//
+//  Created by Михаил Иванов on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
